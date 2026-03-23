1217d162a4a2ee6b3385c93c166afe8701c02e72
tintin75011 <martin.chassard@gmail.com>
2026-03-23 17:08:30 +0100

    fix gitignore et suppression build install log

--- build/ScanFilter/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/user/lidar_gap_ws/install/ScanFilter'
--- build/urg_node2/CMakeFiles/urg_c.dir/urg_library/current/src/urg_ring_buffer.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/urg_c.dir/urg_library/current/src/urg_ring_buffer.c.o: \
- /home/user/lidar_gap_ws/src/urg_node2/urg_library/current/src/urg_ring_buffer.c \
- /usr/include/stdc-predef.h \
- /home/user/lidar_gap_ws/src/urg_node2/urg_library/current/include/c/urg_ring_buffer.h
